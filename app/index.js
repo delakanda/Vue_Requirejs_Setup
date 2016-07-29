bef2b2cf3a2b1eaf1dce359079b86455cac4e279
Delali Kanda <dkanda@nthc.com.gh>
2016-07-29 11:21:05 +0000

    initial commit of setup

---
+++ app/index.js
@@ -0,0 +1,12 @@
+requirejs(['functions'],
+
+function   (functions) {
+
+    new Vue({
+        el: '#app',
+            data: {
+            message: getMessage()
+        }
+    });
+
+});
---
+++ app/require_config.js
@@ -0,0 +1,9 @@
+// Require Config File
+
+requirejs.config({
+    baseUrl: '../app',
+    paths: {
+        functions: 'modules/functions',
+        index: "index"
+    }
+});
